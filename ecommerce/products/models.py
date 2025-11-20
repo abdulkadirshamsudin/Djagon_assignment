e07541a483feda1f3327465ff8818e2e5098db29
Abdulkadir Shamsudin <abdulkadir.shamsudin@strathmore.edu>
2025-11-20 18:31:40 +0300

    Added Product model
    Added Product model

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length= 200)
-    
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="products"
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.name
